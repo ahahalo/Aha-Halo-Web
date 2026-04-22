af46254fe907fabd8b2be080afd3c716376a776f
Jens Kauteren <koekskeinc@gmail.com>
2026-04-23 00:43:51 +0200

    homeo

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1776863353|6134065852';
+const CACHE_VERSION = '1776897823|28457267';
 /** @type {string} */
 const CACHE_PREFIX = 'Aha Web-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
